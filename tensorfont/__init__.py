1f5a52a4e5a8236aaa65ebcbeea4c60b1986d190
Sebastian Kosch <sebastian@aldusleaf.org>
2019-04-22 18:34:01 -0400

    Add pixel-based size setting

--- tensorfont/__init__.py
+++ tensorfont/__init__.py
@@ -38,19 +38,28 @@ class Font(object):
   Use this to load up a font and begin exploring it.
   """
 
-  def __init__(self, filename):
+  def __init__(self, filename, x_height_in_px):
     self.filename = filename
     self.face = freetype.Face(filename)
-    self.face.set_char_size( 64 * self.face.units_per_EM )
+
+    # Set size by rendering /x and dividing by its height
+    self.face.set_pixel_sizes(0, self.face.units_per_EM) # render at 1 pixel per unit
+    x_height_at_em = self.get_xheight()
+    self.scale_factor = x_height_in_px / x_height_at_em
+    x_height_unit = int(self.face.units_per_EM * self.scale_factor)
+    self.face.set_pixel_sizes(0, x_height_unit)
 
     self.ascender = self.face.ascender
-    """The font's ascender height, in font units."""
+    self.ascender_px = int(self.face.ascender * self.scale_factor)
+    """The font's ascender height, in font units and pixels."""
 
     self.descender = self.face.descender
-    """The font's descender height, in font units (usually negative)."""
+    self.descender_px = int(self.face.descender * self.scale_factor)
+    """The font's descender height, in font units and pixels (usually negative)."""
 
     self.full_height = self.ascender - self.descender
-    """The font's full (descender + ascender) height."""
+    self.full_height_px = self.ascender_px - self.descender_px
+    """The font's full (descender + ascender) height, in font units and pixels."""
 
     self.baseline_ratio = 1 - (self.ascender) / self.full_height
     """The ascender-to-descender ratio."""
@@ -58,7 +67,11 @@ class Font(object):
     self.glyphcache = {}
     self.kernreader = None
 
-  def glyph(self,g):
+  def get_xheight(self):
+    self.face.load_char("x", freetype.FT_LOAD_RENDER | freetype.FT_LOAD_TARGET_MONO)
+    return self.face.glyph.bitmap.rows
+
+  def glyph(self, g):
     """Access a glyph by name. Returns a `Glyph` object."""
     if g in self.glyphcache: return self.glyphcache[g]
     self.glyphcache[g] = Glyph(self,g)
@@ -74,7 +87,7 @@ class Font(object):
     """The height of the 'x' glyph, in font units."""
     return self.glyph("x").height
 
-  def pair_kerning(self,left,right):
+  def pair_kerning(self, left, right):
     """The kerning between two glyphs (specified by name), in font units."""
     if self.face.has_kerning:
       return self.face.get_kerning(left, right).x >> 6
@@ -85,17 +98,17 @@ class Font(object):
         return self.kernreader.kerningPairs[(left,right)]
       return 0
 
-  def pair_distance(self,left,right,with_kerning=True):
+  def pair_distance(self, left, right, with_kerning=True):
     """The ink distance between two named glyphs, in font units.
     This is formed by adding the right sidebearing of the left glyph to the left sidebearing
     of the right glyph, plus a kerning correction. To turn off kerning, use `with_kerning=False`."""
     distance = self.glyph(left).rsb + self.glyph(right).lsb
     if with_kerning:
-      distance = distance + self.pair_kerning(left,right)
+      distance = distance + self.pair_kerning(left, right)
     return distance
 
   def minimum_ink_distance(self,left,right):
-    """The distance between the ink of the left glyph and the ink of the right glyph, when
+    """The distance, in pixels, between the ink of the left glyph and the ink of the right glyph, when
     sidebearings are discarded. For many pairs, this will be zero, as the shapes bump up against
     each other (consider "nn" and "oo"). However, pairs like "VA" and "xT" will have a large
     minimum ink distance."""
@@ -124,7 +137,7 @@ class Glyph(object):
     """The left sidebearing in font units."""
     self.rsb       = ((slot.advance.x >> 6) - slot.bitmap.width - self.lsb)
     """The right sidebearing in font units."""
-    self.tsb       = max(self.font.ascender - slot.bitmap_top,0)
+    self.tsb       = self.font.ascender_px - slot.bitmap_top # could use max(0, ...) here
     """The top sidebearing (distance from ascender to ink top) in font units."""
 
   @lru_cache(maxsize=1000)
@@ -133,8 +146,7 @@ class Glyph(object):
     in the range 0 to 255, but they can be normalized or turned into binary values with the
     appropriate keyword arguments. The matrix is returned as a `GlyphRendering` object which
     can be further manipulated."""
-    box_height = self.font.full_height
-    self.face.set_char_size( 64 * self.face.units_per_EM )
+    box_height = self.font.full_height_px
     self.face.load_char(self.name)
 
     slot = self.face.glyph
@@ -143,12 +155,12 @@ class Glyph(object):
     top = slot.bitmap_top # above-baseline glyph height
     w, h = bitmap.width, bitmap.rows
 
-    y = int(self.tsb) # top-most row to draw on
+    y = max(int(self.tsb), 0) # top-most row to draw on
 
-    visible_height = min(h, box_height - y)
+    visible_height = min(h, box_height - y) - max(-self.tsb, 0)
 
     Z = np.zeros((box_height, w))
-    Z[y:y+visible_height, 0:w] += np.array(bitmap.buffer, dtype='ubyte').reshape(h,w)[0:visible_height, :].astype(np.float)
+    Z[y:y+visible_height, 0:w] += np.array(bitmap.buffer, dtype='ubyte').reshape(h, w)[max(-self.tsb, 0):visible_height+max(-self.tsb, 0), :].astype(np.float)
     if normalize or binarize:
       Z = Z / 255.0
     if binarize:
@@ -165,7 +177,13 @@ class GlyphRendering(np.ndarray):
 
   def with_padding(self, left_padding, right_padding):
     """Returns a new `GlyphRendering` object, left and right zero-padding to the glyph image."""
-    padding = ((0,0),(left_padding,right_padding))
+    padding = ((0,0),(left_padding, right_padding))
+    padded = pad(self, padding, "constant")
+    return GlyphRendering.init_from_numpy(self._glyph, padded)
+
+  def with_padding_to_constant_box_width(self, box_width):
+    padding_width = (box_width - self._glyph.ink_width) / 2.0
+    padding = ((0, 0), (int(np.ceil(padding_width)), int(np.floor(padding_width))))
     padded = pad(self, padding, "constant")
     return GlyphRendering.init_from_numpy(self._glyph, padded)
 
@@ -224,7 +242,7 @@ class GlyphRendering(np.ndarray):
 
   def apply_flexible_distance_kernel(self, strength):
     """Transforms the matrix by applying a flexible distance kernel, with given strength."""
-    transformed = np.clip(strength-ndimage.distance_transform_edt(np.logical_not(self)),0,strength)/strength
+    transformed = 1. - np.clip(strength-ndimage.distance_transform_edt(np.logical_not(self)),0,strength)
     return GlyphRendering.init_from_numpy(self._glyph,transformed)
 
   def gradients(self):
